8394f8bbf7fefb7e860e782de14e8a2c1c4a3662
patelharshn <133669931+patelharshn@users.noreply.github.com>
2024-04-11 17:57:19 +0530

    user delete code add

--- admin/command/user_sql.php
+++ admin/command/user_sql.php
@@ -1,5 +1,5 @@
 <?php
-include('command/conn.php');
+include('conn.php');
 
 session_start();
 if (isset($_POST['btn_user_edit'])) {
@@ -24,4 +24,20 @@ if (isset($_POST['btn_user_edit'])) {
 }
 
 
-// Delete User Panding.....
+if (isset($_POST['yes_btn'])) {
+    $u_id = $_POST['delete_p_id'];
+    $sql = "delete from user where id='$u_id'";
+    $result = mysqli_query($con, $sql);
+    $delete = mysqli_affected_rows($con);
+    if ($delete > 0) {
+        $_SESSION['icon'] = "success";
+        $_SESSION['message'] = "User Deleted Successfully!";
+        header("Location: http://localhost/newproject/admin/user.php");
+        exit();
+    } else {
+        $_SESSION['icon'] = "error";
+        $_SESSION['message'] = "User Delete Failed!";
+        header("Location: http://localhost/newproject/admin/user.php");
+        exit();
+    }
+}
